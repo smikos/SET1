c836020601bcb73cd5db21c6001fe2ad63bef52e
Konstantin Smirnov <sasharugby@yandex.ru>
2024-07-31 21:38:45 +0300

    1222done

--- sem1/Program.cs
+++ sem1/Program.cs
@@ -2,12 +2,20 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 namespace sem1
 {
     class Program
     {
-        static void Server(string name)
+        static private CancellationTokenSource cts = new CancellationTokenSource();
+        static private CancellationToken ct = cts.Token;
+         static string  mems;
+        static void Server()
         {
+            string name = "Server";
+            
+            while (!ct.IsCancellationRequested)
+            { 
             // Инициализация объектов UdpClient и IPEndPoint
             UdpClient udpClient;
             IPEndPoint remoteEndPoint;
@@ -15,82 +23,95 @@ namespace sem1
             udpClient = new UdpClient(12345);
             remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
             Console.WriteLine("UDP Клиент ожидает сообщений...");
-            while (true)
-            {
-                byte[] receiveBytes = udpClient.Receive(ref remoteEndPoint);
-                string receivedData = Encoding.ASCII.GetString(receiveBytes);
-                try
+                while (true)
                 {
-
-                    var message = Message.FromJson(receivedData); // Десериализация сообщения из JSON
-
-
-                    if (message.Text != "1234")
+                    byte[] receiveBytes = udpClient.Receive(ref remoteEndPoint);
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+                    try
                     {
-  Console.WriteLine($"Получено сообщение от {message.FromName} ({message.Date}):");
-                    Console.WriteLine(message.Text);
-                        Console.Write("Введите ответ и нажмите Enter: "); // Запрос ответа и отправка ответного сообщения
-                        string replyMessage1 = "1234";
-                        var replyMessageJson1 = new Message()
+
+                        var message = Message.FromJson(receivedData); // Десериализация сообщения из JSON
+
+
+                        if (message.Text != "1234")
+                        {
+                            Console.WriteLine($"Получено сообщение от {message.FromName} ({message.Date}):");
+                            Console.WriteLine(message.Text);
+                            Console.Write("Введите ответ и нажмите Enter: "); // Запрос ответа и отправка ответного сообщения
+                            string replyMessage1 = "1234";
+                            var replyMessageJson1 = new Message()
+                            {
+                                Date = DateTime.Now,
+                                FromName = name,
+                                Text = replyMessage1
+                            }.ToJson();
+                            byte[] replyBytes1 = Encoding.ASCII.GetBytes(replyMessageJson1);
+                            udpClient.Send(replyBytes1, replyBytes1.Length, remoteEndPoint);
+                        }
+                        else if (message.Text == "1234")
+                        {
+                            Console.WriteLine("Сообщение дошло!!!!");
+
+
+                        }
+
+
+
+                        string replyMessage = Console.ReadLine();
+                       mems = replyMessage;
+                        if (mems=="Exit")
+                        {
+                           break;
+                        }    
+                        var replyMessageJson = new Message()
                         {
                             Date = DateTime.Now,
                             FromName = name,
-                            Text = replyMessage1
+                            Text = replyMessage
                         }.ToJson();
-                        byte[] replyBytes1 = Encoding.ASCII.GetBytes(replyMessageJson1);
-                        udpClient.Send(replyBytes1, replyBytes1.Length, remoteEndPoint);
+                        byte[] replyBytes = Encoding.ASCII.GetBytes(replyMessageJson);
+                        udpClient.Send(replyBytes, replyBytes.Length, remoteEndPoint);
+                        Console.WriteLine("Ответ отправлен.");
                     }
-                   else if(message.Text == "1234")
+                    catch (Exception ex)
                     {
-                        Console.WriteLine("Сообщение дошло!!!!");
-
-
+                        Console.WriteLine("Ошибка при обработке сообщения: " + ex.Message);
                     }
-                  
-
-                    
-                    string replyMessage = Console.ReadLine();
-                    var replyMessageJson = new Message()
-                    {
-                        Date = DateTime.Now,
-                        FromName = name,
-                        Text = replyMessage
-                    }.ToJson();
-                    byte[] replyBytes = Encoding.ASCII.GetBytes(replyMessageJson);
-                    udpClient.Send(replyBytes, replyBytes.Length, remoteEndPoint);
-                    Console.WriteLine("Ответ отправлен.");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Ошибка при обработке сообщения: " + ex.Message);
-                }
+                } 
             }
+            ct.ThrowIfCancellationRequested();
         }
       
       
 
-        static void Main(string[] args)
-        {/*
-            Console.WriteLine("Enter some Integer");
-            if (args.Length == 1)
+        static async Task Main(string[] args)
+        {
+           
+            Task t = new Task(Server,ct);
+            t.Start();
+            while (true)
+            {  string d = Console.ReadLine();
+                if (d == "Exit")
+                {
+                    cts.Cancel();
+                    break;
+                        }
+                 }
+            Console.WriteLine("Задача завершена");
+            try
             {
-                // Если указан только один аргумент, запускается сервер
-                Server("d");
+                await t;
             }
-            else if (args.Length == 2)
+            catch(OperationCanceledException e)
             {
-                // Если указано два аргумента, запускается клиент
-               Client(args[0], args[1]);
+                if(t.IsCanceled)
+                {
+                    Console.WriteLine("Задача завершена");
+                }
             }
-            else
-            {
-                // Если указано неверное количество аргументов, выводится информация о запуске
-                Console.WriteLine("Для запуска сервера введите ник-нейм как параметр запуска приложения");
-                Console.WriteLine("Для запуска клиента введите ник-нейм и IP сервера как параметры запуска приложения");
-            }
-            */
-           Server("Сервер");
-            //Client("DOSD", "127.0.0.1");
+
+                
+           
         }
     }
 }
